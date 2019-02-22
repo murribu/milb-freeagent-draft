2dbf7ccf8becafe74b88d6b871e239e98c9377a0
Cory Martin <murribu@gmail.com>
2019-02-21 20:40:31 -0600

    ability to ad pick players

--- src/Components/MyPicks.js
+++ src/Components/MyPicks.js
@@ -1,19 +1,25 @@
 import React from "react";
-import { Form } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 import players from "../players.json";
+import "./MyPicks.css";
 
 class MyPicks extends React.Component {
   constructor(props) {
     super(props);
     this.handleSearchChange = this.handleSearchChange.bind(this);
     this.handleSortChange = this.handleSortChange.bind(this);
+    this.renderSelectedPlayer = this.renderSelectedPlayer.bind(this);
+    this.selectPlayer = this.selectPlayer.bind(this);
+    this.pickThisPlayer = this.pickThisPlayer.bind(this);
   }
 
   state = {
     players: players,
     sortColumn: "last_name",
     sortDirection: "asc",
-    search: ""
+    search: "",
+    selectedPlayer: {},
+    picks: {}
   };
 
   filteredPlayers() {
@@ -38,6 +44,14 @@ class MyPicks extends React.Component {
       );
   }
 
+  sortedPicks() {
+    var ordered = {};
+    Object.keys(this.state.picks)
+      .sort()
+      .forEach(k => (ordered[k] = this.state.picks[k]));
+    return ordered;
+  }
+
   handleSearchChange(e) {
     this.setState({ search: e.target.value });
   }
@@ -56,19 +70,96 @@ class MyPicks extends React.Component {
     }
   }
 
+  pickThisPlayer(e) {
+    var player_ba_id = e.currentTarget.attributes["data-player"].value;
+    var player = this.state.players.find(
+      p => p.baseballamerica === player_ba_id
+    );
+    var picks = { ...this.state.picks };
+    var ordinal = 1;
+    for (var p = 1; p <= 20; p++) {
+      if (typeof picks[p] === "undefined") {
+        ordinal = p;
+        break;
+      }
+    }
+    picks[ordinal] = player;
+    this.setState({ picks });
+  }
+
+  selectPlayer(e) {
+    var filteredPlayers = this.filteredPlayers();
+    var player = filteredPlayers[e.currentTarget.attributes["data-key"].value];
+    console.log(player);
+    this.setState({ selectedPlayer: player });
+  }
+
+  renderSelectedPlayer() {
+    if (typeof this.state.selectedPlayer.first_name !== "undefined") {
+      return (
+        <div className="text-center">
+          <h3>
+            {this.state.selectedPlayer.first_name +
+              " " +
+              this.state.selectedPlayer.last_name}
+          </h3>
+          <p className="lead">{this.state.selectedPlayer.team}</p>
+          <Button
+            onClick={this.pickThisPlayer}
+            data-player={this.state.selectedPlayer.baseballamerica}
+          >
+            Pick this Player
+          </Button>
+        </div>
+      );
+    } else {
+      return "";
+    }
+  }
+
+  renderPicks() {
+    var ret = [];
+    for (var p = 1; p <= 20; p++) {
+      ret.push(
+        <div className="row">
+          <div className="col-1 text-right">{p}.</div>
+          <div className="col-10">
+            {this.state.picks[p]
+              ? this.state.picks[p].first_name +
+                " " +
+                this.state.picks[p].last_name
+              : ""}
+          </div>
+        </div>
+      );
+    }
+    return ret;
+  }
+
   render() {
     var filteredPlayers = this.filteredPlayers();
     return (
       <div className="container">
         <div className="row">
-          <div className="col-sm-6 col-xs-12" />
-          <div className="col-sm-6 col-xs-12">
+          <div className="col-sm-4 col-xs-12">
+            <div className="container">
+              <h2>My Picks</h2>
+              {this.renderPicks()}
+            </div>
+          </div>
+          <div className="col-sm-4 col-xs-12">
+            {this.renderSelectedPlayer()}
+          </div>
+          <div className="col-sm-4 col-xs-12">
             <Form.Control
               placeholder="search"
               defaultValue={this.state.search}
               onChange={this.handleSearchChange}
             />
-            <div className="container">
+            <div
+              className="container"
+              style={{ maxHeight: "500px", overflowY: "scroll" }}
+            >
               <div className="row">
                 <div className="col-6" style={{ fontWeight: "bold" }}>
                   <a
@@ -90,7 +181,12 @@ class MyPicks extends React.Component {
                 </div>
               </div>
               {Object.keys(filteredPlayers).map(key => (
-                <div className="row" key={key}>
+                <div
+                  className={"row " + (key % 2 === 0 ? "gray" : "")}
+                  key={key}
+                  data-key={key}
+                  onClick={this.selectPlayer}
+                >
                   <div className="col-6">
                     {filteredPlayers[key].first_name +
                       " " +
