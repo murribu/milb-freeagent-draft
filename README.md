e2df43d43de20a037313c90049d935480431e2d9
Cory Martin <murribu@gmail.com>
2019-03-01 14:05:02 -0600

    about page

--- README.md
+++ README.md
@@ -1,11 +1,1 @@
-Trying to combine https://github.com/aws-samples/aws-amplify-graphql and https://github.com/mwarger/amplify-auth-examples
-
-1. `git clone`
-1. \$ `npm install -g @aws-amplify/cli` (If you don't already have amplify installed)
-1. \$ `amplify configure` (If you haven't already configured amplify)
-1. \$ `amplify init`
-1. \$ `amplify add auth`
-1. \$ `amplify add api`
-1. \$ `amplify push`
-1. \$ `yarn`
-1. \$ `cp config.example.js config.js` And then put your FB app's ID in there
+A detailed tutorial is forthcoming...
--- src/Components/AboutComponent.js
+++ src/Components/AboutComponent.js
@@ -14,7 +14,41 @@ class AboutComponent extends React.Component {
   };
 
   render() {
-    return <div>Keep it AboutComponent, dude!</div>;
+    return (
+      <div className="container">
+        <h1>Hi!</h1>
+        <p className="lead">
+          I hope you're enjoying the MiLB Free Agent Draft 2019
+        </p>
+        <p>
+          During Spring Training, the stats will be from Spring Training. Once
+          the regular season starts, you will no longer be able to edit your
+          picks, and the stats will reset and only come from regular season MLB
+          games.
+        </p>
+        <p>
+          This project was developed by{" "}
+          <a href="https://twitter.com/murribu">Cory Martin</a>.<br />
+          It uses ReactJS and a serverless backend, powered by AWS.
+        </p>
+        <p>
+          You can view the source code, submit issues, and peruse a tutorial of
+          how you could build something similar by visiting{" "}
+          <a href="https://github.com/murribu/milb-freeagent-draft">
+            the github page
+          </a>{" "}
+          for this project
+        </p>
+        <p>
+          If you are in the market for some help with a software need, please
+          consider reaching out to{" "}
+          <a href="https://dozensoft.com/contact" target="_new">
+            Dozen Software
+          </a>{" "}
+          (my employer) and tell us your software dreams.
+        </p>
+      </div>
+    );
   }
 }
 
